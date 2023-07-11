14d4ddc625f5555a18b3cef173c454cb2581e395
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-11 23:41:06 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../style";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        Tes Lagu A
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
