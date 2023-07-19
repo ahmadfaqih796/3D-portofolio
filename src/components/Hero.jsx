13680fab5716ead382676eef685660df3b291882
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-19 10:44:01 +0700

    House Canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { ComputersCanvas } from "./canvas";
 import { styles } from "@/app/styles";
+import HouseCanvas from "./canvas/House";
 
 const Hero = () => {
   return (
@@ -24,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <HouseCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
