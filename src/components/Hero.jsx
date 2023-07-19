cc2c289709f5483e253d57073e69f29bb0b5e369
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-19 11:00:08 +0700

    hoc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
       <HouseCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
